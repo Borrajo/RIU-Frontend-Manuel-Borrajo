eeeccb8d3063f76ca37fb9e01ce126d1fd0367f7
Manuel Borrajo <borrajo@hotmail.com.ar>
2026-09-16 10:33:12 -0300

    Add delete hero confirm dialog

---
+++ src/app/delete-hero-dialog/delete-hero-dialog.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogContent, MatDialogClose, MatDialogTitle } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-hero-dialog',
+  imports: [
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogClose,
+    MatDialogTitle,
+    MatButtonModule
+  ],
+  templateUrl: './delete-hero-dialog.html',
+  styleUrl: './delete-hero-dialog.scss',
+})
+export class DeleteHeroDialog { }
